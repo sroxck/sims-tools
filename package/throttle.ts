77f1b298f8d0115c1a2acf30aa03c4ba3a8e9a16
Sims <51316180+sroxck@users.noreply.github.com>
2022-07-15 14:03:29 +0800

    enhance throttle func

--- package/throttle.ts
+++ package/throttle.ts
@@ -1,11 +1,28 @@
-
-export default function throttle(cb: Function,delay:number){
-  let time:any = 0
-  return function(){
-    let now  = Date.now()
-    if(now - time > delay){
-        cb && cb.apply(this,arguments)
-        time  = now
+export default function throttle(
+  cb: Function,
+  delay: number,
+  trailing: boolean
+) {
+  let time: any = 0;
+  let timer: NodeJS.Timeout | number | null;
+  return function () {
+    let now = Date.now();
+    if (now - time > delay) {
+      if (cb) {
+        cb.apply(this, arguments);
+      }
+      time = now;
     }
-  }
+    // 是否开启尾执行
+    if ( trailing) {
+      if (timer) {
+        clearTimeout(timer);
+        timer = null;
+      }
+      timer = setTimeout(() => {
+        cb.apply(this, arguments);
+        time = now;
+      }, delay);
+    }
+  };
 }
