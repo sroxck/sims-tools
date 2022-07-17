02a3c871ba4021112c240dec3e512f70fa4a235c
sims <642261686@qq.com>
2022-07-17 16:46:33 +0800

    add findMin

--- package/findMin.ts
+++ package/findMin.ts
@@ -6,29 +6,29 @@ interface ArrayType {
 }
 /**
  * 此方法返回数组或对象数组中给定key的最大值
- * findMax([1,2,3]) => 3
+ * findMin([1,2,3]) => 3
  * const obj = [{name:'zs',age:18},{name:'zs2',age:19} ]
- * findMax(obj,'age') => 19
- * findMax(obj,'age',true) => {name:'zs2',age:19}
+ * findMin(obj,'age') => 19
+ * findMin(obj,'age',true) => {name:'zs2',age:19}
  * @param value
  * @param name
  */
-export default function findMax<T extends ArrayType | number>(
+export default function findMin<T extends ArrayType | number>(
   value: T[],
   name?: string,
   root?: boolean
 ): number | object | Error {
   if (!isArray(value)) throw new TypeError("参数为number[] | object[]");
   if (name === noop()) {
-    return Math.max.apply(Math, value as unknown as number[]);
+    return Math.min.apply(Math, value as unknown as number[]);
   }
   if (name && root) {
     // 这里需要更优雅，原生的解决方案
     const copy = JSON.parse(JSON.stringify(value))
-    copy.sort((item: any, old: any) => (<ArrayType>old)[name] - (<ArrayType>item)[name])
+    copy.sort((item: any, old: any) => (<ArrayType>item)[name] - (<ArrayType>old)[name])
     return value = copy[0]
   } else {
-    if (isNaN(Math.max.apply(Math, value.map(item => (<ArrayType>item)[name])))) throw new TypeError("参数类型不可被排序")
-    return Math.max.apply(Math, value.map(item => (<ArrayType>item)[name]))
+    if (isNaN(Math.min.apply(Math, value.map(item => (<ArrayType>item)[name])))) throw new TypeError("参数类型不可被排序")
+    return Math.min.apply(Math, value.map(item => (<ArrayType>item)[name]))
   }
 }
--- test/findMax.spec.ts
+++ test/findMax.spec.ts
@@ -1,13 +1,25 @@
 import {findMax} from '../package/index'
+import {findMin} from '../package/index'
 
-test('findMAx',()=>{
+test('findMax and min',()=>{
   // let s = [2,3,4,5]
   let s1 = [{
     name:"3",
     age:33
   },{
-    name:"3333单独",
+    name:"54",
+    age:343
+  },{
+    name:444,
     age:343
   }]
-  expect(findMax(s1,'name')).toBe(8)
+  expect(findMax(s1,'name',true)).toEqual({
+    name:444,
+    age:343
+  })
+  
+  expect(findMin(s1,'name',true)).toEqual({
+    name:"3",
+    age:33
+  })
 })
