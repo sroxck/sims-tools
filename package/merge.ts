471630f0cc797f869ce0f9d2b177eb6536ae2096
sims <642261686@qq.com>
2022-07-24 09:06:27 +0800

    完成merge函数

--- package/merge.ts
+++ package/merge.ts
@@ -15,16 +15,26 @@
  * @param key 合并的键
  * @param value 合并的值
  */
-export default function merge(_list: Record<string,any>[],_key:string,_value:number | string) {
-  // const map:Record<string,any> = {}
-  // const result = []
-  // for (const [i,v] of list.entries()) {
+export default function merge(list: Record<string, any>[], _key: string, _value: number | string) {
+  const map:any = {}
+  const res:any = []
+  let index = -1
+  while(++index < list.length){
+    const item = list[index]
+    if(!map[item[_key]]){
+      res.push(item)
+      map[item[_key]] = item
+    }else{
+      let i = -1
+      while(++i < res.length){
+        const val = res[i]
+        if(val[_key] === item[_key]){
+          val[_value] =parseFloat( val[_value])+ parseFloat(item[_value])
+          break
+        }
+      }
+    }
     
-  // }
+  }
+  return res
 }
-// var arr = [
-//   {"id":"1","name":"张三","value":"1111"},
-//   {"id":"1","name":"张三","value":"1111"},
-//   {"id":"2","name":"李四","value":"2222"},
-//   {"id":"2","name":"李四","value":"2222"}]
-//   merge(arr,'id','value')
